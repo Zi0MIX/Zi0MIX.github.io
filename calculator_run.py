5b2d03cbb0b788737f901580f8dfd71677dcad63
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2022-11-26 09:12:42 +0100

    Moved module import to a function

--- calculator_run.py
+++ calculator_run.py
@@ -1,18 +1,30 @@
 from js import Alpine, Object, console
+from sys import exit
 from pyodide.ffi import create_proxy, to_js
 
-try:
-    from calculator_web import MAP_LIST, map_translator, main
-except ModuleNotFoundError as err:
-    module_error = [{"type": "error", "message": str(err)}]
-    console.log(str(module_error))
-    output_data = to_js(module_error, dict_converter = Object.fromEntries)
-    Alpine.store('calculatorOutput', output_data)
 
+def import_calculator() -> tuple:
+    try:
+        from calculator_web import main, map_translator, MAP_LIST
+        return (main, map_translator, MAP_LIST)
+    except ModuleNotFoundError as err:
+        module_error = [{"type": "error", "message": str(err)}]
+        console.log(str(module_error))
+        output_data = to_js(module_error, dict_converter = Object.fromEntries)
+        Alpine.store('calculatorOutput', output_data)
+        return False
+
+
+calculator = import_calculator()
+if not calculator:
+    exit()
+
+main, map_translator, MAP_LIST = calculator[0], calculator[1], calculator[2]
 
 # Expose calculator_web imports to Alpine
 Alpine.store('mapCodes', to_js(MAP_LIST))
 Alpine.store('mapTranslator', create_proxy(map_translator))
+
 
 # Perform calculation and use Alpine.store() for I/O
 def run_calculator():
