dc72fd381f690980fd19f64ba2ea0c93ede0ed54
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2022-11-26 05:01:00 +0100

    Merge from RZ

--- calculator_run.py
+++ calculator_run.py
@@ -1,21 +1,15 @@
-import js, json
-from js import Alpine
-from pyodide.ffi import to_js
+from js import Alpine, Object, console
+from pyodide.ffi import create_proxy, to_js
+from calculator_web import MAP_LIST, map_translator, main
 
-# Expose some Python globals to JavaScript and Alpine
-js.pyGlobals = globals()
+# Expose calculator_web imports to Alpine
 Alpine.store('mapCodes', to_js(MAP_LIST))
-
-# Pass print inputs to JavaScript console and debug view, handled by Alpine
-def print(text = ""):
-    output = to_js(text, dict_converter = js.Object.fromEntries)
-    js.console.log(output)
-    Alpine.store('calculatorOutput', output)
+Alpine.store('mapTranslator', create_proxy(map_translator))
 
 # Perform calculation and use Alpine.store() for I/O
 def run_calculator():
     input_data = Alpine.store('calculatorInput').to_py()
     calculation_data = main(input_data)
-    js.console.log(str(calculation_data))
-    output_data = to_js(calculation_data, dict_converter = js.Object.fromEntries)
+    console.log(str(calculation_data))
+    output_data = to_js(calculation_data, dict_converter = Object.fromEntries)
     Alpine.store('calculatorOutput', output_data)
