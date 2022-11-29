5436c13deaa3c4229327ac3506d44ad618c60a97
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2022-11-29 03:19:38 +0100

    Merge from RZ & adjustments

--- calculator_run.py
+++ calculator_run.py
@@ -1,17 +1,23 @@
-from js import Alpine, Object, console
+from js import Alpine, Object, console, performance
 from sys import exit
 from pyodide.ffi import create_proxy, to_js
 
 
+# Exports Python to JavaScript console and Alpine.store()
+def export_data(data):
+    # console.log(str(data))
+    output = to_js(data, dict_converter = Object.fromEntries)
+    Alpine.store('calculatorOutput', output)
+    Alpine.store('time').end = performance.now()
+
+
 def import_calculator() -> tuple:
     try:
-        from calculator_web import main, map_translator, MAP_LIST
-        return (main, map_translator, MAP_LIST)
+        from calculator_web import main_api, map_translator, MAP_LIST, get_arguments
+        return (main_api, map_translator, MAP_LIST, get_arguments)
     except ModuleNotFoundError as err:
         module_error = [{"type": "error", "message": str(err)}]
-        console.log(str(module_error))
-        output_data = to_js(module_error, dict_converter = Object.fromEntries)
-        Alpine.store('calculatorOutput', output_data)
+        export_data(module_error)
         return False
 
 
@@ -19,17 +25,15 @@ calculator = import_calculator()
 if not calculator:
     exit()
 
-main, map_translator, MAP_LIST = calculator[0], calculator[1], calculator[2]
+main, map_translator, MAP_LIST, get_arguments = calculator[0], calculator[1], calculator[2], calculator[3]
 
 # Expose calculator_web imports to Alpine
 Alpine.store('mapCodes', to_js(MAP_LIST))
 Alpine.store('mapTranslator', create_proxy(map_translator))
-
+Alpine.store('arguments', to_js(get_arguments(), dict_converter = Object.fromEntries))
 
 # Perform calculation and use Alpine.store() for I/O
 def run_calculator():
     input_data = Alpine.store('calculatorInput').to_py()
     calculation_data = main(input_data)
-    console.log(str(calculation_data))
-    output_data = to_js(calculation_data, dict_converter = Object.fromEntries)
-    Alpine.store('calculatorOutput', output_data)
+    export_data(calculation_data)
